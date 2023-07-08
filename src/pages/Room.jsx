42a879157881a2f89bcb9a333d767e99bbe1a2c7
Kristjan Lääne <xonproject@gmail.com>
2023-07-08 15:25:56 +0300

    basic styling

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,22 +1,44 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import {
   databases,
   DATABASE_ID,
   COLLECTION_ID_MESSAGES,
-} from "../appwriteConfig";
+} from '../appwriteConfig'
 
 export const Room = () => {
+  const [messages, setMessages] = useState([])
+
   useEffect(() => {
-    getMessages();
-  }, []);
+    getMessages()
+  }, [])
 
   const getMessages = async () => {
     const response = await databases.listDocuments(
       DATABASE_ID,
       COLLECTION_ID_MESSAGES
-    );
-    console.log(response);
-  };
+    )
+    console.log(response)
+    setMessages(response.documents)
+  }
 
-  return <div>Room</div>;
-};
+  return (
+    <main className='container'>
+      <div>
+        <div className='room--container'>
+          {messages.map((messages) => (
+            <div key={messages.$id} className='message--wrapper'>
+              <div className='message--header'>
+                <small className='message-timestamp'>
+                  {messages.$createdAt}
+                </small>
+              </div>
+              <div className='message--body'>
+                <span>{messages.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  )
+}
