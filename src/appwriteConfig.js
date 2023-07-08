ffd4db28ca2db2fc8070d8c4edb573a5d4f73e85
Kristjan Lääne <xonproject@gmail.com>
2023-07-08 15:07:52 +0300

    querying db items

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,15 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
+
+export const PROJECT_ID = "64a94a813aee0b66e9ff";
+export const DATABASE_ID = "64a94c6991818d1e0f84";
+export const COLLECTION_ID_MESSAGES = "64a94c73512afc9a7761";
 
 const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("64a94a813aee0b66e9ff");
+
+export const databases = new Databases(client);
 
 export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGES,
+} from "../appwriteConfig";
 
 export const Room = () => {
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES
+    );
+    console.log(response);
+  };
+
   return <div>Room</div>;
 };
