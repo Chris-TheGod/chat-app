42f1a0fb69135bed48cc141e78b550a9b951939e
Kristjan Lääne <xonproject@gmail.com>
2023-07-08 18:51:20 +0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import "./App.css";
-import { Room } from "./pages/Room";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import { LoginPage } from './pages/LoginPage'
+import { Room } from './pages/Room'
+import { PrivateRoutes } from './components/PrivateRoutes'
 
 function App() {
   return (
-    <>
-      <Room></Room>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path='/' element={<Room />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
