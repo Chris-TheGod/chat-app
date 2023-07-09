b2a12154faadf4cef8a3192dce3ccedf067dfded
Kristjan Lääne <xonproject@gmail.com>
2023-07-09 15:59:59 +0300

    login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuth } from '../utils/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
@@ -6,11 +6,54 @@ export const LoginPage = () => {
   const { user } = useAuth()
   const navigate = useNavigate()
 
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  })
+
   useEffect(() => {
     if (user) {
       navigate('/')
     }
   }, [])
 
-  return <div>LoginPage</div>
+  const handleInputChange = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+
+    setCredentials({ ...credentials, [name]: value })
+    console.log(credentials)
+  }
+
+  return (
+    <div className='auth--container'>
+      <div className='form-wrapper'>
+        <form>
+          <div className='field-wrapper'>
+            <label>Email:</label>
+            <input
+              type='email'
+              required
+              name='email'
+              placeholder='Enter your email...'
+              value={credentials.email}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className='field-wrapper'>
+            <label>Password:</label>
+            <input
+              type='password'
+              required
+              name='password'
+              placeholder='Enter your password...'
+              value={credentials.password}
+              onChange={handleInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
